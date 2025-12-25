70052280bea39734e5b4991ec1f6db6f67c6430a
Karan <karanap86@gmail.com>
2025-12-25 05:49:10 +0530

    Add Tailwind and PostCSS config for correct styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./main.jsx",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
